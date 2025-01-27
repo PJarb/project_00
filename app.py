2685bc2be87cd6cdc7bdf0fb56ab8ff3a5915e2a
PJarb <w.pachinee@gmail.com>
2025-01-27 10:16:07 +0000

    bb is here

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import streamlit as st
 import random
 
 st.title('Test streamlit')
-st.write('Hello World!')
+st.write('Hello Worldd!')
 
 if st.button ('Generate Random Number'):
     random_number = random.randint(1,100)
